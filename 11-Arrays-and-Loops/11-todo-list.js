52349d42d7ea36dab2570daea88aa51498c98ba2
Juan Carlos Martinez <martinez2@southwestern.edu>
2024-07-09 21:53:50 -0600

    Finished 11-Arrays-and-Loops

--- 11-Arrays-and-Loops/11-todo-list.js
+++ 11-Arrays-and-Loops/11-todo-list.js
@@ -8,12 +8,22 @@ function renderTodoList() {
 
     // This technique is called Generating the HTMl
     for (let i = 0; i < todoList.length; i++) {
-        const todo = todoList[i];
-        const html = `<p>${todo}</p>`;
+        const todoObject = todoList[i];
+        // const name = todoObject.name;
+        const { name, dueDate } = todoObject;
+        // const dueDate = todoObject.dueDate;
+        const html = `
+        <div>${name}</div>
+        <div>${dueDate}</div>
+        <button class="delete-todo-button" onclick="
+            todoList.splice(${i}, 1);
+            renderTodoList();
+        ">Delete</button>
+        `;
         // We need to generate a big HTML code
         todoListHTML += html;
     }
-    console.log(todoListHTML);
+
     document.querySelector('.js-todo-list').innerHTML = 
     todoListHTML;
 }
@@ -22,9 +32,15 @@ function renderTodoList() {
 function addTodo() {
     const inputElement = document.querySelector('.js-name-input');
     const name = inputElement.value;
+    const dateInputElement = document.querySelector('.js-due-date-input');
+    const dueDate = dateInputElement.value;
     
-    todoList.push(name);
-    console.log(todoList);
+    todoList.push({
+        // name: name,
+        // dueDate: dueDate
+        name,
+        dueDate
+    });
 
     // The text box clean the text after add it
     inputElement.value = '';
