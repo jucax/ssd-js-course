28c5d094c0911d0c967bbb9cddc83f4193b110be
Juan Carlos Martinez <martinez2@southwestern.edu>
2024-07-10 17:05:43 -0600

    Started 12-Advanced-Functions

---
+++ 12-Advanced-Functions/12-todo-list.js
@@ -0,0 +1,64 @@
+
+const todoList = [];
+renderTodoList();
+
+// Render means to display something in the page
+function renderTodoList() {
+    let todoListHTML = '';
+
+    todoList.forEach(function(todoObject, index) {
+        const { name, dueDate } = todoObject;
+        const html = `
+        <div>${name}</div>
+        <div>${dueDate}</div>
+        <button class="delete-todo-button" onclick="
+            todoList.splice(${index}, 1);
+            renderTodoList();
+        ">Delete</button>
+        `;
+        // We need to generate a big HTML code
+        todoListHTML += html;
+    });
+
+    /*
+    // This technique is called Generating the HTMl
+    for (let i = 0; i < todoList.length; i++) {
+        const todoObject = todoList[i];
+        // const name = todoObject.name;
+        const { name, dueDate } = todoObject;
+        // const dueDate = todoObject.dueDate;
+        const html = `
+        <div>${name}</div>
+        <div>${dueDate}</div>
+        <button class="delete-todo-button" onclick="
+            todoList.splice(${i}, 1);
+            renderTodoList();
+        ">Delete</button>
+        `;
+        // We need to generate a big HTML code
+        todoListHTML += html;
+    }
+    */
+
+    document.querySelector('.js-todo-list').innerHTML = 
+    todoListHTML;
+}
+
+
+function addTodo() {
+    const inputElement = document.querySelector('.js-name-input');
+    const name = inputElement.value;
+    const dateInputElement = document.querySelector('.js-due-date-input');
+    const dueDate = dateInputElement.value;
+    
+    todoList.push({
+        // name: name,
+        // dueDate: dueDate
+        name,
+        dueDate
+    });
+
+    // The text box clean the text after add it
+    inputElement.value = '';
+    renderTodoList();
+}
