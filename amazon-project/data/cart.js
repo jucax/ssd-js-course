3be5a4de8677a68a06ee71cb5b29a4526ed20ada
Juan Carlos Martinez <martinez2@southwestern.edu>
2024-08-30 21:40:45 +0100

    Generated HTML for the order summary

--- amazon-project/data/cart.js
+++ amazon-project/data/cart.js
@@ -1,8 +1,13 @@
 // export allows the variable to be use outsite the file
-export const cart = [
-    
-];
+export const cart = [{
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2
+}, {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1
+}];
 
+// Export the finction so we can use it in other file
 export function addToCart(productId) {
     let matchingItem;
 
