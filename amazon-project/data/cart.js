5d971d2a04f1b2253d00e05a050a4565b9717a09
Juan Carlos Martinez <martinez2@southwestern.edu>
2024-08-31 20:28:24 +0100

    Load cart from local storage

--- amazon-project/data/cart.js
+++ amazon-project/data/cart.js
@@ -1,11 +1,22 @@
 // export allows the variable to be use outsite the file
-export let cart = [{
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2
-}, {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1
-}];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+// If there is nothing in local storage cart is null, and then we save the default values
+if (!cart) {
+    cart = [{
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 2
+    }, {
+        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 1
+    }];
+}
+
+
+// Save the cart content in local storage so it doesnt reload when we refresh the page
+function saveToStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 // Export the finction so we can use it in other file
 export function addToCart(productId) {
@@ -30,6 +41,7 @@ export function addToCart(productId) {
             quantity: quantitySelector
         });
     }
+    saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -43,4 +55,5 @@ export function removeFromCart(productId) {
         }
     });
     cart = newCart;
+    saveToStorage();
 }
