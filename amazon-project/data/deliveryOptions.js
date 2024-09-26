ec730f2eaa9ea94a0ab11fbd7d632b1ede5b6589
Juan Carlos Martinez <martinez2@southwestern.edu>
2024-09-26 21:43:49 +0100

    Created function to skip weekend days when display delivery date

--- amazon-project/data/deliveryOptions.js
+++ amazon-project/data/deliveryOptions.js
@@ -26,10 +26,25 @@ export function getDeliveryOption(deliveryOptionId) {
     return deliveryOption || deliveryOptions[0];
 }
 
+function isWeekend(date) {
+    const dayOfWeek = date.format('dddd');
+    // If the say is weekend
+    return dayOfWeek === 'Saturday' || dayOfWeek === 'Sunday';
+}
+
 export function calculateDeliveryDate(deliveryOption) {
-    // Get the preset value with dayjs()
-    const today = dayjs();
-    const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
+    let remainingDays = deliveryOption.deliveryDays;
+    let deliveryDate = dayjs();
+
+    while (remainingDays > 0) {
+        deliveryDate = deliveryDate.add(1, 'day');
+
+        // If the day is not weeekend, then we substract 1 in remaining days
+        if (!isWeekend(deliveryDate)) {
+            remainingDays--;
+        }
+    }
+
     // Use the format rules from the documentation
     const dateString = deliveryDate.format('dddd, MMMM D');
 
