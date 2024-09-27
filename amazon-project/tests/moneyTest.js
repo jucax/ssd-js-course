0a0244b6b6e947f75d4dc50ce506c9a283127f99
Juan Carlos Martinez <martinez2@southwestern.edu>
2024-09-27 22:07:16 +0100

    Created automated tests for formatCurrency function

---
+++ amazon-project/tests/moneyTest.js
@@ -0,0 +1,34 @@
+import {formatCurrency} from '../scripts/utils/money.js'
+
+console.log('Test suite: formatCurrency');
+console.log('Converts cents into dollars');
+
+// First automated test for the format currency function
+// Basic test:
+if (formatCurrency(2095) === '20.95') {
+    console.log('passed');
+} else {
+    console.log('failed');
+}
+
+console.log('Works with zero');
+
+// We can do different tests for different situations
+if (formatCurrency(0) === '0.00') {
+    console.log('passed')
+} else {
+    console.log('failed')
+}
+
+console.log('Rounds up to the nearest cent');
+
+// Edge test:
+if (formatCurrency(2000.5) === '20.01') {
+    console.log('passed');
+} else {
+    console.log('failed');
+}
+
+// Basic test cases: tests if code is working
+// Edge cases: tests with tricky values
+// Group of related test is called a test suite
