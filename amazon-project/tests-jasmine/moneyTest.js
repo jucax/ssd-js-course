eac07222604de36b3574e792696c60b05a6273d6
Juan Carlos Martinez <martinez2@southwestern.edu>
2024-09-29 21:51:38 +0100

    Removed default tests and added money tests

--- amazon-project/tests-jasmine/moneyTest.js
+++ amazon-project/tests-jasmine/moneyTest.js
@@ -5,7 +5,15 @@ describe('Test suite: formatCurrency', () => {
     // it() creates a test
     it('Converts cents into dollars', () => {
         // expect() compare a value to other value, so we dont have to use the if statement
-        // toEqual() is like use the '===' comparision
+        // because expect gives an oject, we use toEqual() like '===' to make the comparision
         expect(formatCurrency(2095)).toEqual('20.95');
+    });
+
+    it('Works with 0', () => {
+        expect(formatCurrency(0)).toEqual('0.00')
+    });
+
+    it('Rounds up to the nearest cent', () => {
+        expect(formatCurrency(2000.5)).toEqual('20.01')
     });
 });
