fd87c3ee4410af073cfdecb0b6f3ad19103088c9
Juan Carlos Martinez <martinez2@southwestern.edu>
2024-10-02 23:48:44 +0100

    Updated code

--- amazon-project/tests-jasmine/data/cartTest.js
+++ amazon-project/tests-jasmine/data/cartTest.js
@@ -9,8 +9,8 @@ describe('Test suite: addToCart', () => {
     // Flaky test, because if passes depending on what is saved in local storage
     it('Adds a new product to the cart', () => {
         // Create a mock, a fake version of a function to make whatever we need
-        
-        // Jasmine use spyOn to create a mock, the first parameter is the object, the second is the method that we want to mock
+
+        // Jasmine use spyOn to create a mock, the first parameter is the object, the second is the method that we want to mock, and the content inside is the new functionality
         spyOn(localStorage, 'getItem').and.callFake(() => {
             // We override the function with this instructions
             return JSON.stringify([]);
