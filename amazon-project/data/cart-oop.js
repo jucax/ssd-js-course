3c35e5f9278d02246705db1be4e9c790baff3b85
Juan Carlos Martinez <martinez2@southwestern.edu>
2024-10-09 22:02:50 +0100

    Created cart-opps.js file

--- amazon-project/data/cart-oop.js
+++ amazon-project/data/cart-oop.js
@@ -1,32 +1,64 @@
 import {validDeliveryOption} from './deliveryOptions.js';
 
-// Export allows the variable to be use outsite the file
-export let cart;
+// Main object
+const cart = {
+    // We can't use export inside an object, so we convert it into a propery
+    // This is how we get a variable into an object
+    cartItems: undefined,
+
+    // This is how we get a function into an object, and becomes a method
+    loadFromStorage() {
+        // We can use "this" instead of a variable name, it means the outer object
+        this.cartItems = JSON.parse(localStorage.getItem('cart-oop'));
+    
+        if (!this.cartItems) {
+            this.cartItems = [{
+                productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+                quantity: 2,
+                deliveryOptionId: '1'
+            }, {
+                productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+                quantity: 1,
+                deliveryOptionId: '2'
+            }];
+        }
+    },
+
+    saveToStorage() {
+        localStorage.setItem('cart-oop', JSON.stringify(this.cartItems));
+    },
+
+    addToCart(productId) {
+        let matchingItem;
+    
+        this.cartItems.forEach((cartItem) => {
+            if (productId === cartItem.productId) {
+                matchingItem = cartItem;
+            }
+        });
+    
+        const quantitySelector = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
+    
+        if (matchingItem) {
+            matchingItem.quantity += quantitySelector;
+        } else {
+            this.cartItems.push({
+                productId: productId,
+                quantity: quantitySelector,
+                deliveryOptionId: '1'
+            });
+        }
+
+        // If we want to acces a function that is inside the object, we need to access the object first
+        this.saveToStorage();
+    }
+};
+
+
 
 loadFromStorage();
 
-export function loadFromStorage() {
-    cart = JSON.parse(localStorage.getItem('cart'));
 
-    // If there is nothing in local storage cart is null, and then we save the default values
-    if (!cart) {
-        cart = [{
-            productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-            quantity: 2,
-            deliveryOptionId: '1'
-        }, {
-            productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-            quantity: 1,
-            deliveryOptionId: '2'
-        }];
-    }
-}
-
-
-// Save the cart content in local storage so it doesnt reload when we refresh the page
-function saveToStorage() {
-    localStorage.setItem('cart', JSON.stringify(cart));
-}
 
 // Export the finction so we can use it in other file
 export function addToCart(productId) {
