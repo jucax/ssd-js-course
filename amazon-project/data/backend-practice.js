63f7d38e2a02374011ef9041e435de540b674792
Juan Carlos Martinez <martinez2@southwestern.edu>
2024-10-30 23:01:00 +0000

    Done practice xhr requests and responses

--- amazon-project/data/backend-practice.js
+++ amazon-project/data/backend-practice.js
@@ -2,8 +2,16 @@
 // This creates a HTTP message/request
 const xhr = new XMLHttpRequest();
 
+// load means to wait for the response
+xhr.addEventListener('load', () => {
+    console.log(xhr.response);
+});
+
 // We configure the messge with open() 
 // First parameter is the type of message, GET = get info from backend
 // Second parameter is where to send the message, we need a URL
 xhr.open('GET', 'https://supersimplebackend.dev');
 xhr.send();
+
+// This messages works with the request-response cycle
+// We can do something with the response with .response, it is asynchronous code, so it doesn't wait for the last lines to execute
