a29bcfbf8a9ee2b2b0d1cf02bd1e280e73a3889b
Juan Carlos Martinez <martinez2@southwestern.edu>
2024-11-08 20:50:27 +0000

    Created tracking.js

--- amazon-project/data/orders.js
+++ amazon-project/data/orders.js
@@ -10,3 +10,15 @@ export function addOrder(order) {
 function saveToStorage() {
     localStorage.setItem('orders', JSON.stringify(orders));
 }
+
+export function getOrder(orderId) {
+    let matchingOrder;
+  
+    orders.forEach((order) => {
+        if (order.id === orderId) {
+            matchingOrder = order;
+        }
+    });
+  
+    return matchingOrder;
+}
